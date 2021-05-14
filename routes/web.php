c27ab5bb140530ce8bc32152771edff3f728fce8
AKRAM <mami.akram@live.fr>
2021-05-14 20:34:03 +0100

    route fix

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 //public accesse
 Route::get('/','App\Http\Controllers\FrontController@profileShow')->name('profile');
+Route::get('projcet/{id}/{title}','App\Http\Controllers\ProjectController@showProject')->name('showProject');
 
 // accesse for non logged users ->middleware('auth')
 Route::middleware(['guest'])->group(function () {
@@ -25,7 +26,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('admin/service/edit/{id}','App\Http\Controllers\ServiceController@editService')->name('editService');
     Route::post('admin/service/update','App\Http\Controllers\ServiceController@updateService')->name('updateService');
     Route::post('admin/service/delete', 'App\Http\Controllers\ServiceController@deleteService')->name('deleteService');
-    Route::get('projcet/{id}/{title}','App\Http\Controllers\ProjectController@showProject')->name('showProject');
     Route::get('admin/project/add', 'App\Http\Controllers\ProjectController@addProject')->name('addProject');
     Route::post('admin/project/save', 'App\Http\Controllers\ProjectController@saveProject')->name('saveProject'); 
     Route::get('admin/project/edit/{id}','App\Http\Controllers\ProjectController@editProject')->name('editProject'); 
